50ce9fb793c288a5600953f13d72760e259cc45b
阳君 <937447974@qq.com>
2016-04-18 17:12:15 +0800

    项目搭建

---
+++ YJLogFactory/YJLogFactory/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  YJLogFactory
+//
+//  Created by admin on 16/4/18.
+//  Copyright © 2016年 YJFactory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
