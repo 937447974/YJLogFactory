bdd5faa1b397d87409798cad03c3f043369de633
阳君 <937447974@qq.com>
2016-04-18 17:21:16 +0800

    代码上传

---
+++ YJLogFactory/YJLogFactory/Classes/NSLog+YJLogFactory.h
@@ -0,0 +1,27 @@
+//
+//  NSLog+YJLogFactory.h
+//  YJLogFactory
+//
+//  CSDN:http://blog.csdn.net/y550918116j
+//  GitHub:https://github.com/937447974/Blog
+//
+//  Created by 阳君 on 16/4/18.
+//  Copyright © 2016年 YJFactory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/** 数组NSLog打印扩展*/
+@interface NSArray (YJLogFactory)
+
+@end
+
+/** 字典NSLog打印扩展*/
+@interface NSDictionary (YJLogFactory)
+
+@end
+
+/** NSSet打印扩展*/
+@interface NSSet (YJLogFactory)
+
+@end
--- YJLogFactory/YJLogFactory/main.m
+++ YJLogFactory/YJLogFactory/main.m
@@ -9,9 +9,16 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
+    
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSArray *array = [NSArray arrayWithObjects:@"阳君", nil];
+        NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:array, @"name", @"937447974", @"qq", nil];
+        NSSet *set = [NSSet setWithObjects:@"937447974", @"阳君", dict, nil];
+        array = [NSArray arrayWithObjects:@"阳君", dict, set, nil];
+        dict = [NSDictionary dictionaryWithObjectsAndKeys:array, @"name", @"937447974", @"qq", nil];
+        NSLog(@"%@", dict);
+        
     }
     return 0;
+    
 }
